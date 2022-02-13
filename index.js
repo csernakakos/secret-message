446f1f9915b0c6ec2cbce64d8737190a136ea6b3
csernakakos <csernakakos@gmail.com>
2022-02-13 21:17:33 +0100

    Created message sharing app on flight from BCN to CPH

---
+++ index.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+app.use(cors());
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true}));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+let encrypted = "";
+let decrypted = "";
+const port = 3100;
+
+
+app.get("/", (req, res) => {
+    res.status(200).redirect("/new-message")
+});
+
+app.get("/new-message", (req, res) => {
+    res.status(200).render("new-message", {
+        title: "Enter a secret message"
+    })
+});
+
+app.use("/message", (req, res, next) => {  
+    encrypted = btoa(req.body.message);
+    decrypted = atob(encrypted);
+    next();
+});
+
+app.post("/message", (req, res) => {
+    res.status(201).render("message", {
+        title: "Message created!",
+        encrypted: encrypted,
+        decrypted: decrypted,
+        url: `http://${req.hostname}:${port}/${encrypted}`
+    });
+});
+
+
+app.use(`/${encrypted}`, (req, res) => {
+    res.render("decrypted", {
+        title: "Your secret message",
+        encrypted: encrypted,
+        decrypted: decrypted,
+    })
+})
+
+app.listen(port, () => {console.log("listening...")})
